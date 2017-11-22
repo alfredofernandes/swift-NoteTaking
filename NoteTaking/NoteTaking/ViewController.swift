e8406c7e73354a65ed4cd372344c42ac15a7649d
Alfredo Fernandes <alfredorfernandes@gmail.com>
2017-11-22 12:07:07 -0500

    - Added tableView;

--- NoteTaking/NoteTaking/ViewController.swift
+++ NoteTaking/NoteTaking/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    var data: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
